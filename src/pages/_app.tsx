c443c68ab4622da91a4d020388089155614d11aa
Helen Andrade <helenandradeoliveira21@gmail.com>
2025-08-26 15:04:57 -0300

    feat: add readme

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+import logoImg from "../assets/logo.svg";
+
+import Image from "next/image";
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import { GetStaticProps } from "next";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import { stripe } from "../lib/stripe";
+
+import "keen-slider/keen-slider.min.css";
+
+import Stripe from "stripe";
+
+import { styled } from "@stitches/react";
+import Link from "next/link";
+import { HomeContainer, Product } from "../styles/pages/home";
+import Head from "next/head";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[];
+}
+
+const Button = styled("button", {
+  backgroundColor: "$green300",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+              prefetch={false}
+            >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </HomeContainer>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format((price.unit_amount ?? 0) / 100),
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours,
+  };
+};
